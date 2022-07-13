18e4b651a3e12712fa5633ea94a4de428a97db46
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2022-07-13 13:05:13 +0530

    :tada: init

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import twitterLogo from "./assets/twitter-logo.svg";
+import "./App.css";
+
+// Constants
+const TWITTER_HANDLE = "_arihantbansal";
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+	return (
+		<div className="App">
+			<div className="container">
+				<div className="header-container">
+					<p className="header">🖼 Darth Fandom</p>
+					<p className="sub-text">
+						View your GIF collection in the metaverse ✨
+					</p>
+				</div>
+				<div className="footer-container">
+					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+					<a
+						className="footer-text"
+						href={TWITTER_LINK}
+						target="_blank"
+						rel="noreferrer">{`built on @${TWITTER_HANDLE}`}</a>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default App;
