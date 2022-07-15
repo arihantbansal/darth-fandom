40d83af016dd7310a20830a3a41668f51e643720
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2022-07-15 16:49:24 +0530

    :sparkles: update

--- anchor/programs/fandom/src/lib.rs
+++ anchor/programs/fandom/src/lib.rs
@@ -6,9 +6,22 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod fandom {
     use super::*;
     pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs = 0;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct StartStuffOff {}
+pub struct StartStuffOff<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
---
+++ anchor/tests/fandom.js
@@ -0,0 +1,40 @@
+const anchor = require("@project-serum/anchor");
+
+const { SystemProgram } = anchor.web3;
+
+const main = async () => {
+	console.log("🚀 Starting test...");
+
+	const provider = anchor.AnchorProvider.env();
+	anchor.setProvider(provider);
+
+	const program = anchor.workspace.Fandom;
+
+	const baseAccount = anchor.web3.Keypair.generate();
+
+	const tx = await program.rpc.startStuffOff({
+		accounts: {
+			baseAccount: baseAccount.publicKey,
+			user: provider.wallet.publicKey,
+			systemProgram: SystemProgram.programId,
+		},
+		signers: [baseAccount],
+	});
+
+	console.log("📝 Your transaction signature", tx);
+
+	let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+	console.log("👀 GIF Count", account.totalGifs.toString());
+};
+
+const runMain = async () => {
+	try {
+		await main();
+		process.exit(0);
+	} catch (error) {
+		console.error(error);
+		process.exit(1);
+	}
+};
+
+runMain();
